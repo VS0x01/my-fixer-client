acc1857f7c0bec2b39f0e80d1583fc1362f39cce
Vadim <vadim.a.shesterikov@gmail.com>
2019-08-15 11:24:44 +0300

    make menu items to be router links

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
+    <AppHeader v-if="authorized" />
+    <AppMenu v-if="authorized" />
     <router-view />
   </div>
 </template>
 
 <script>
+import AppHeader from "@/components/AppHeader";
+import AppMenu from "@/components/AppMenu";
+
 export default {
-  name: "app"
+  name: "app",
+  components: { AppMenu, AppHeader },
+  data() {
+    return {
+      authorized: true,
+    }
+  }
 };
 </script>
 
--- src/router.js
+++ src/router.js
@@ -1,10 +1,13 @@
 import VueRouter from 'vue-router';
 import SignIn from '@/auth/views/SignIn';
 import SignUp from '@/auth/views/SignUp';
+import MessengerComponent from "@/messenger/views/Messenger";
 
 const routes = [
+  { path: '/', redirect: '/sign-in' },
   { path: '/sign-in', component: SignIn },
   { path: '/sign-up', component: SignUp },
+  { path: '/messenger', component: MessengerComponent },
 ];
 
 const router = new VueRouter({
