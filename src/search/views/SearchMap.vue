54761ff7c51c14194f0875cb63834f37bb2847fc
Vadim <vadim.a.shesterikov@gmail.com>
2019-08-21 13:38:49 +0300

    add Google maps styles

--- src/search/views/SearchMap.vue
+++ src/search/views/SearchMap.vue
@@ -11,7 +11,8 @@
       :center="{ lat: 10, lng: 10 }"
       :zoom="7"
       map-type-id="terrain"
-      style="margin: 17px 41px; flex: 1 1 auto"
+      style="margin: 17px 41px; flex: 1 1 auto; border: 2px solid #EFEFEF;"
+      :options="{ styles: mapStyles }"
     >
       <GmapMarker
         :key="index"
@@ -27,7 +28,181 @@
 
 <script>
 export default {
-  name: "SearchMap"
+  name: "SearchMap",
+  data() {
+    return {
+      mapStyles: [
+        {
+          featureType: "all",
+          elementType: "geometry.fill",
+          stylers: [
+            {
+              weight: "2.00"
+            }
+          ]
+        },
+        {
+          featureType: "all",
+          elementType: "geometry.stroke",
+          stylers: [
+            {
+              color: "#9c9c9c"
+            }
+          ]
+        },
+        {
+          featureType: "all",
+          elementType: "labels.text",
+          stylers: [
+            {
+              visibility: "on"
+            }
+          ]
+        },
+        {
+          featureType: "landscape",
+          elementType: "all",
+          stylers: [
+            {
+              color: "#f2f2f2"
+            }
+          ]
+        },
+        {
+          featureType: "landscape",
+          elementType: "geometry.fill",
+          stylers: [
+            {
+              color: "#ffffff"
+            }
+          ]
+        },
+        {
+          featureType: "landscape.man_made",
+          elementType: "geometry.fill",
+          stylers: [
+            {
+              color: "#ffffff"
+            }
+          ]
+        },
+        {
+          featureType: "poi",
+          elementType: "all",
+          stylers: [
+            {
+              visibility: "off"
+            }
+          ]
+        },
+        {
+          featureType: "road",
+          elementType: "all",
+          stylers: [
+            {
+              saturation: -100
+            },
+            {
+              lightness: 45
+            }
+          ]
+        },
+        {
+          featureType: "road",
+          elementType: "geometry.fill",
+          stylers: [
+            {
+              color: "#eeeeee"
+            }
+          ]
+        },
+        {
+          featureType: "road",
+          elementType: "labels.text.fill",
+          stylers: [
+            {
+              color: "#7b7b7b"
+            }
+          ]
+        },
+        {
+          featureType: "road",
+          elementType: "labels.text.stroke",
+          stylers: [
+            {
+              color: "#ffffff"
+            }
+          ]
+        },
+        {
+          featureType: "road.highway",
+          elementType: "all",
+          stylers: [
+            {
+              visibility: "simplified"
+            }
+          ]
+        },
+        {
+          featureType: "road.arterial",
+          elementType: "labels.icon",
+          stylers: [
+            {
+              visibility: "off"
+            }
+          ]
+        },
+        {
+          featureType: "transit",
+          elementType: "all",
+          stylers: [
+            {
+              visibility: "off"
+            }
+          ]
+        },
+        {
+          featureType: "water",
+          elementType: "all",
+          stylers: [
+            {
+              color: "#46bcec"
+            },
+            {
+              visibility: "on"
+            }
+          ]
+        },
+        {
+          featureType: "water",
+          elementType: "geometry.fill",
+          stylers: [
+            {
+              color: "#c8d7d4"
+            }
+          ]
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.fill",
+          stylers: [
+            {
+              color: "#070707"
+            }
+          ]
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.stroke",
+          stylers: [
+            {
+              color: "#ffffff"
+            }
+          ]
+        }
+      ]
+    };
+  }
 };
 </script>
 
